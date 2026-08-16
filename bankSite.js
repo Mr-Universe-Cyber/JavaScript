b96789e71e479871f7603ffb1160a8558f7932ff
Mr-Universe-Cyber <solomonagua@gmail.com>
2026-08-15 20:38:29 -0400

    menu toggle

--- bankSite.js
+++
@@ -1,5 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-menuToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-});
